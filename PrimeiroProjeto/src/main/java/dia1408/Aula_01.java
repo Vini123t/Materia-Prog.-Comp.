422d04e24f38748458ff0b84bb8dd7782626cb42
Vini123t <111025380+Vini123t@users.noreply.github.com>
2024-08-14 20:47:20 -0300

    dia 01

---
+++ PrimeiroProjeto/src/main/java/dia1408/Aula_01.java
@@ -0,0 +1,21 @@
+package dia1408;
+
+/**
+ *
+ * @author vinid
+ */
+public class Aula_01 {
+    
+    public static void main(String [] args){
+        String nome;
+        
+//         System.out.println("Olá mundo !");
+//        System.out.println("Apos o Println!");
+//        System.out.println(" printl = joga o cursosr para a linha de baixo.");
+
+    nome = "Vinicius Duarte";
+
+        System.out.println("Olá " + nome);
+    }
+    
+}
